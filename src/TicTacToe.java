a492f227ebf79be98dc9501357ea722fc3f677f5
Sondre Espe <Sondreespe02@gmail.com>
2024-03-28 12:08:17 +0100

    first commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    
+
+
+    TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free", Font.BOLD, 75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("TicTacToe");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0, 0, 800, 100);
+
+        button_panel.setLayout(new GridLayout(3,3));
+        button_panel.setBackground(new Color(150,150,150));
+
+        for(int i = 0; i < 9; i++){
+            buttons[i] = new JButton();
+            button_panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+
+        }
+        
+        title_panel.add(textfield);
+        frame.add(title_panel, BorderLayout.NORTH);
+        frame.add(button_panel); 
+
+        firstTurn();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public void firstTurn(){
+        try{
+            Thread.sleep(4000);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+        if(random.nextInt(2)==1){
+            player1_turn = true;
+            textfield.setText("X Turn");
+
+        }else{
+            player1_turn = false;
+            textfield.setText("O Turn");
+        }
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
+    
+}
